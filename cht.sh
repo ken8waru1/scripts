cb0a96272a14a406ed7daeba6bd78b53fad0022c
Ken Ha <ken8waru1@gmail.com>
2024-04-09 06:05:10 -0700

    Fix custom script path and add nodejs to cht.sh searchable languages

--- cht.sh
+++ cht.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-languages=$(echo "golang c cpp javascript typescript rust" | tr " " "\n")
+languages=$(echo "golang c cpp nodejs javascript typescript rust" | tr " " "\n")
 core_utils=$(echo "find xargs sed awk" | tr " " "\n")
 
 selected=$(echo -e "$languages\n$core_utils" | fzf)
